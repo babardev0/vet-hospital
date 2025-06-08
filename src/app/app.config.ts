da58dd4f88d3592a47ab511ad5376aae9e34f64c
babardev0 <babar.dev0@gmail.com>
2025-06-08 15:57:06 +0500

    t1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,31 +1,34 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
-import { TranslationsService } from './core/services/translations.service';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideRouterStore } from '@ngrx/router-store';
 
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
     provideHttpClient(),
-    provideAnimations(),
-    provideStore(),
-    provideEffects(),
-    provideRouterStore(),
     importProvidersFrom(
       TranslateModule.forRoot({
-        defaultLanguage: 'en',
-        useDefaultLang: true
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
       })
     ),
-    TranslationsService
+    provideStore(),
+    provideEffects(),
+    provideRouterStore()
   ]
 };
